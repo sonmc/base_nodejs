8ec5cbd5106830346ea42bd9d68e3938b8831199
SonMc <maicongson@zen8labs.com>
2023-05-12 12:13:12 +0700

    add migration and design permission

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,18 +1,25 @@
-import { getRepository } from "typeorm";
-import { User } from "../infrastructure/schemas/User";
+import { getRepository } from 'typeorm';
+import { User } from '../infrastructure/schemas/user.schema';
+import { compare, getUserByToken } from './bcrypt.service';
 
 class UserService {
-  userRepo;
-  constructor() {
-    this.userRepo = getRepository(User);
-  }
+    userRepo;
+    constructor() {
+        this.userRepo = getRepository(User);
+    }
 
-  async register(firstName: string, email: string) {
-    const user = new User();
-    user.firstName = firstName;
-    user.email = email;
-    return this.userRepo.save(user);
-  }
+    async getUser(refresh_token: string) {
+        const username = getUserByToken(refresh_token);
+        console.log('user', username);
+
+        const user = await this.userRepo.getUserByUsername(username);
+
+        const isRefreshTokenMatching = await compare(refresh_token, user.hash_refresh_token);
+        if (isRefreshTokenMatching) {
+            return user;
+        }
+        return null;
+    }
 }
 
 export default new UserService();
